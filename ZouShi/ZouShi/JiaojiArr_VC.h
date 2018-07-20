1d9e87f726787d560c15e71ea4ba2fd8c7b53d9b
chengguozhi <theorangeview@163.com>
2018-07-20 09:37:27 +0800

    增加重复分布图

---
+++ ZouShi/ZouShi/JiaojiArr_VC.h
@@ -0,0 +1,13 @@
+//
+//  JiaojiArr_VC.h
+//  ZouShi
+//
+//  Created by 程国治 on 2018/7/20.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface JiaojiArr_VC : ViewController
+
+@end
---
+++ ZouShi/ZouShi/JiaojiArr_VC.m
@@ -0,0 +1,37 @@
+//
+//  JiaojiArr_VC.m
+//  ZouShi
+//
+//  Created by 程国治 on 2018/7/20.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import "JiaojiArr_VC.h"
+
+@interface JiaojiArr_VC ()
+
+@end
+
+@implementation JiaojiArr_VC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
